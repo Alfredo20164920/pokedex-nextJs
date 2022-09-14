1208f68ae920510a9f6b518c9bf129cff13ae75d
Alfredo20164920 <apaz20164920@gmail.com>
2022-09-13 22:12:30 -0500

    Add dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
