a997eddd2e7eb1f47d854955fd3e492e961c51c4
Alfredo20164920 <apaz20164920@gmail.com>
2022-09-16 13:16:16 -0500

    Crete page to favorite pokemon

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Spacer, Text, useTheme } from '@nextui-org/react'
+import { Link, Spacer, Text, useTheme } from '@nextui-org/react'
 import Image from 'next/image';
+import NextLink from 'next/link';
 import React from 'react'
 
 export const Navbar = () => {
@@ -16,20 +17,26 @@ export const Navbar = () => {
             padding: '0px 20px',
             backgroundColor: theme?.colors.gray200.value,
         }}>
-
             <Image 
                 src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
                 alt="Pokemon Logo"
                 width={ 50 }
                 height={ 50 }
             />
-
-            <Text color='white'h2>P</Text>
-            <Text color='white'>okémon</Text>
+            <NextLink href={'/'}>
+                <Link>
+                    <Text color='white'h2>P</Text>
+                    <Text color='white'>okémon</Text>
+                </Link>
+            </NextLink>
 
             <Spacer css={{ flex: 1 }}/>
         
-            <Text color='white'>Favoritos</Text>
+            <NextLink href={'/favoritos'}>
+                <Link>
+                    <Text color='white' >Favoritos</Text>
+                </Link>
+            </NextLink>
         </div>
     )
 }
---
+++ pages/favoritos/index.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from "../../components/Layouts"
+
+const Favoritos = () => {
+    return (
+        <MainLayout title="Favoritos">
+
+        </MainLayout>
+    )
+}
+
+export default Favoritos
